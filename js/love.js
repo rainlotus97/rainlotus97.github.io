3754fe1e637d08de818ef8c3990845de012b2ac7
rainlotus97 <s0704127x@163.com>
2024-01-27 23:45:22 +0800

    Site updated: 2024-01-27 23:45:22

--- js/love.js
+++ js/love.js
@@ -1,9 +1,6 @@
 // 设置页脚博主会动的心
-document
-  .querySelector(".copyright")
-  .html(
-    '©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> RainLotus'
-  );
+document.querySelector(".copyright").innerHTML =
+  `©2020 - ${new Date().getFullYear()} <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> RainLotus`;
 
 // 移除页脚样式
 var app = document.getElementById("footer");
